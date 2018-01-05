052641d81ccabf2e7e6b554d83721452d9874330
alan <mdx3906@163.com>
2018-01-05 17:41:57 +0800

    增加统计gradle任务执行时间的代码

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# RxJavaPractice
+# GradlePlugPratice
+
+感谢[如何使用Android Studio开发Gradle插件](http://blog.csdn.net/sbsujjbcy/article/details/50782830)
+参考[gradle文档](https://docs.gradle.org/current/userguide/custom_plugins.html)
+
+v1.0.0 主要是最原始的输出字符串功能
+
+v1.1.0 增加统计gradle任务执行时间的代码
--- plug/src/main/groovy/test/gradle/plug/PluginImpl.groovy
+++ plug/src/main/groovy/test/gradle/plug/PluginImpl.groovy
@@ -5,6 +5,9 @@ import org.gradle.api.Project
 
 public class PluginImpl implements Plugin<Project> {
     void apply(Project project) {
+
+        project.gradle.addListener(new TimeListener())
+
         project.task('testTask')  {
             doLast {
                 println "+++++++++++Hello gradle plugin++++++++++"
